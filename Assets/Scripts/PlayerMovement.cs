f9529e0f0220b883d3ebea158f839dd635d6cef0
dtvgaming214 <dtvgaming214@gmail.com>
2025-09-12 18:57:40 +0700

    [FEAT] Add new audio

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+}
+
+
