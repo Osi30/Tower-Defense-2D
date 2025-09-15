113432a0fa4b52052ac7b042d5e17d2b429e12ed
Osi30 <nthanhn2491@gmail.com>
2025-09-15 22:01:48 +0700

    [FEAT] Add node and select tower panel

---
+++ Assets/Scripts/Node/NodeControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class NodeControl : MonoBehaviour
+{
+    [SerializeField]
+    private CanvasGroup _panelCanvas;
+    [SerializeField]
+    private GameObject _placeToChoose;
+    [SerializeField]
+    private DefenseTowerData _towerData;
+
+
+    public void OpenChoicePanel()
+    {
+        _panelCanvas.alpha = 1;
+    }
+
+    public void CloseChoicePanel()
+    {
+        _panelCanvas.alpha = 0;
+    }
+
+    public void ChooseTower(int id)
+    {
+        _placeToChoose.SetActive(false);
+        CloseChoicePanel();
+        GameObject.Instantiate(_towerData.GetTowerById(id), _placeToChoose.transform.position, _placeToChoose.transform.rotation);
+        
+    }
+}
